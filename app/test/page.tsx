788f006981d2ba35dc292e393ae05db2cadd7c00
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-27 20:09:38 -0500

    test page

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function TestPage() {
+  return;
+  <div>
+    Hello
+    <h1 className="text-6xl font-bold bg-clip-text text-transparent bg-linear-to-r from-blue-400 via-purple-400 to-pink-400 mb-3">
+      Weather Dashboard
+    </h1>
+    <p className="text-xl text-gray-300">Real-time weather for all monitored locations</p>
+  </div>;
+}
