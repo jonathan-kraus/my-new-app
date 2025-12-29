5a307d53a101237f1e88c4e70f8a0715b45ea6fe
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-29 00:26:06 -0500

    d

--- prisma.config.cjs
+++ prisma.config.cjs
@@ -4,6 +4,5 @@ console.log("PRISMA CONFIG EXECUTED");
 module.exports = defineConfig({
 	schema: "./prisma/schema.prisma",
 	datasource: {
-		url: "postgresql://neondb_owner:npg_70cnhasCPfuA@ep-nameless-wind-adjj77xh-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-	},
+		datasourceUrl: process.env.DATABASE_URL?.replace(/\/\/[^@]*@/, '//***:***@')
 });
