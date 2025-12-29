60faacf161966d24d3858d84b146c088979117cc
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-29 18:41:09 -0500

    log6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,18 @@
 // app/api/auth/[...nextauth]/route.ts ← EXPORT authOptions
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { appLog } from '@/lib/logger';
+import { appLog } from "@/lib/logger";
 
 export const authOptions = {
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.AUTH_SECRET!,
+	providers: [
+		GitHub({
+			clientId: process.env.AUTH_GITHUB_ID!,
+			clientSecret: process.env.AUTH_GITHUB_SECRET!,
+		}),
+	],
+	secret: process.env.AUTH_SECRET!,
 };
-await appLog({ level: 'info', message: 'Prisma v7 working!', page: 'auth' });
+appLog({ level: "info", message: "Prisma v7 working!", page: "auth" });
 
 const handler = NextAuth(authOptions);
 
