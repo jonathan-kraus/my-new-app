e4e11fa3aa9cedf44ba1080a46712eb316fe7c13
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-29 18:43:57 -0500

    log7

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,4 +1,5 @@
 // app/api/ping/route.ts - SIMPLE TEST
+import { appLog } from "@/lib/logger";
 export async function GET() {
   appLog({ level: "info", message: "ping", page: "init" });
   return Response.json({ message: "API works!" });
