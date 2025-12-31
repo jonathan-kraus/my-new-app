9676eceb48dfcf9377e1193f60fe40981219e843
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-31 01:55:47 -0500

    c7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-// app/layout.tsx
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "./components/Sidebar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="flex">
