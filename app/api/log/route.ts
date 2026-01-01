8ead2da7e8915a5ea131ec220012c6101af5f88c
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-01 03:51:23 -0500

    z7

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,25 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getAuth } from '@/lib/auth';
 import { db } from "@/lib/db";
-const auth = getAuth();
+
 export async function POST(req: NextRequest) {
   try {
-  
-    const authSession = await auth.api.getSession();
-
-    if (!authSession) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
     const body = await req.json();
 
     await db.log.create({
       data: {
         level: body.level,
         message: body.message,
-        userId: authSession.user.id,
-        page: body.page,
-        data: body.data,
+        userId: body.userId ?? null,
+        page: body.page ?? null,
+        data: body.data ?? null,
+        createdAt: new Date(body.createdAt),
       },
     });
 
