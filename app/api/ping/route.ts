42e803e2d6241cf7ac21445d9f2ae5e26a749f48
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-01 04:25:48 -0500

    j1

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -9,8 +9,8 @@ export function GET() {
 		db: process.env.DATABASE_URL ?? "missing",
 	});
 }
-export async function log() {
-	appLog({ level: "info", message: "ping", page: "init" });
-	return NextResponse.json({ message: "API works!" });
-}
-log(); // Simple ping route to test if API is working
+// export async function log() {
+// 	appLog({ level: "info", message: "ping", page: "init" });
+// 	return NextResponse.json({ message: "API works!" });
+// }
+// log(); // Simple ping route to test if API is working
