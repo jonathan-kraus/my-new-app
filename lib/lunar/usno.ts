2fdd3a625ed813bf7be8980a706d059550772f38
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-08 23:28:58 -0500

    r1

--- lib/lunar/usno.ts
+++ lib/lunar/usno.ts
@@ -1,14 +1,14 @@
+import { _isoTime } from "better-auth";
+import { logit } from "@/lib/log/server";
 export interface USNOMoonData {
   moonrise: string | null; // "2026-01-08 16:00:00"
   moonset: string | null; // "2026-01-08 22:00:00"
 }
+// * Fetch accurate moonrise/moonset from USNO
+// * @param latitude number
+// * @param longitude number
+// * @param date Date (local date)
 
-/**
- * Fetch accurate moonrise/moonset from USNO
- * @param latitude number
- * @param longitude number
- * @param date Date (local date)
- */
 export async function getUSNOMoonData(
   latitude: number,
   longitude: number,
@@ -19,7 +19,19 @@ export async function getUSNOMoonData(
   const dd = String(date.getDate()).padStart(2, "0");
 
   const url = `https://api.usno.navy.mil/rstt/oneday?date=${mm}/${dd}/${yyyy}&coords=${latitude},${longitude}&tz=0`;
-
+ await logit({
+    level: "info",
+    message: "Getting data from USNO",
+    file: "lib/lunar/usno.ts",
+    line: 22,
+    page: "USNO",
+    data: {
+      url: url,
+      yyyy: yyyy,
+      mm: mm,
+      dd: dd,
+    },
+  });
   const res = await fetch(url);
   if (!res.ok) {
     console.error("USNO lunar fetch failed", await res.text());
@@ -27,7 +39,17 @@ export async function getUSNOMoonData(
   }
 
   const data = await res.json();
-
+ await logit({
+    level: "info",
+    message: "Getting data from USNO",
+    file: "lib/lunar/usno.ts",
+    line: 42,
+    page: "USNO",
+    data: {
+      data: data,
+      dd: _isoTime,
+    },
+  });
   // USNO returns something like:
   // { "moondata": [ { "phen": "R", "time": "16:00" }, { "phen": "S", "time": "22:00" } ] }
 
