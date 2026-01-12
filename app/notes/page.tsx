029b7373b1a0684a3d009ba83f3a037ceeeeff4d
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-12 03:05:03 -0500

    m6

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,32 +3,32 @@ import { getAuth } from "@/lib/auth";
 import NotesClient from "./NotesClient";
 import { headers } from "next/headers";
 import { logit } from "@/lib/log/server";
+import BlueDashboardLayout from "@/components/layouts/BlueDashboardLayout";
+
 export const dynamic = "force-dynamic";
 
 export default async function NotesPage() {
   const auth = getAuth();
   const h = await headers();
+
   await logit({
     level: "info",
     message: "In Notes page",
     page: "Notes",
     file: "app/notes/page.tsx",
     line: 11,
-    data: {
-      h: h,
-      auth: auth,
+    data: { h, auth }
+  });
 
-    }
-  });
-  const session = await auth.api.getSession({
-    headers: h,
-  });
+  const session = await auth.api.getSession({ headers: h });
 
   return (
-    <NotesClient
-      authorized={!!session?.user}
-      userId={session?.user?.id ?? null}
-      notes={null}
-    />
+    <BlueDashboardLayout>
+      <NotesClient
+        authorized={!!session?.user}
+        userId={session?.user?.id ?? null}
+        notes={null}
+      />
+    </BlueDashboardLayout>
   );
 }
