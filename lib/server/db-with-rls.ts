6210e26b145678d71b9010b4addf961e3d020cf6
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-12 02:55:04 -0500

    m5

--- lib/server/db-with-rls.ts
+++ lib/server/db-with-rls.ts
@@ -2,23 +2,19 @@
 import { neon } from "@neondatabase/serverless";
 import { logit } from "../log/server";
 
-export async function getDbWithRls(email: string) {
-  await logit({
-    level: "info",
-    message: "In getDbWithRls",
-    file: "lib/server/db-with-rls.ts",
-    page: "Notes app",
-    data: { email },
-  });
-
-  const sql = neon(process.env.DATABASE_URL!, {
-    fetchOptions: {
-      headers: {
-        // ONLY this header is allowed
-        "X-User-Email": email,
-      },
-    },
-  });
-
-  return sql;
+export function getDbWithRls(email: string) {
+	logit({
+		level: "info",
+		message: "In getDbWithRls",
+		file: "lib/server/db-with-rls.ts",
+		page: "Notes app",
+		data: { email },
+	});
+	return neon(process.env.DATABASE_URL!, {
+		fetchOptions: {
+			headers: {
+				"app.current_user_email": email, // RLS header
+			},
+		},
+	});
 }
