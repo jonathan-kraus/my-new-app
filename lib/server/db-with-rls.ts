d23a3497ba57c34c385e97af6f69ef4f8b180195
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-12 03:10:55 -0500

    m7

--- lib/server/db-with-rls.ts
+++ lib/server/db-with-rls.ts
@@ -1,20 +1,12 @@
 // lib/server/db-with-rls.ts
 import { neon } from "@neondatabase/serverless";
-import { logit } from "../log/server";
 
 export function getDbWithRls(email: string) {
-	logit({
-		level: "info",
-		message: "In getDbWithRls",
-		file: "lib/server/db-with-rls.ts",
-		page: "Notes app",
-		data: { email },
-	});
-	return neon(process.env.DATABASE_URL!, {
-		fetchOptions: {
-			headers: {
-				"app.current_user_email": email, // RLS header
-			},
-		},
-	});
+  return neon(process.env.DATABASE_URL!, {
+    fetchOptions: {
+      headers: {
+        "app.current_user_email": email,
+      },
+    },
+  });
 }
