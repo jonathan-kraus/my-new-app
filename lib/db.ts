3004170eba63ae780916e9700b5093d81becda57
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-15 01:38:45 -0500

    c6

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,14 @@
-export { db } from "./prisma";
+// lib/db.ts
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
