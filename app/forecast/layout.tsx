f558f3faeb0160db302bffc74ca4a8b7620efce3
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-17 12:23:12 -0500

    v7

--- app/forecast/layout.tsx
+++
@@ -1,6 +0,0 @@
-// app/forecast/layout.tsx
-import ClientLayout from "@/app/ClientLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>;
-}
