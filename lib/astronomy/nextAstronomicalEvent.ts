5a809840bcc42f1e52f75b61fa76576486217bc0
jonathan-kraus <jonathanckraus@gmail.com>
2026-01-17 13:23:25 -0500

    d1

--- lib/astronomy/nextAstronomicalEvent.ts
+++ lib/astronomy/nextAstronomicalEvent.ts
@@ -6,43 +6,39 @@ export function getUnifiedNextEvent(solar: any, lunar: any) {
       type: "solar",
       event: "Sunrise",
       time: solar.sunrise,
-      ms: solar.sunriseMs,
     },
     {
       type: "solar",
       event: "Sunset",
       time: solar.sunset,
-      ms: solar.sunsetMs,
     },
     {
       type: "lunar",
       event: "Moonrise",
       time: lunar.moonrise,
-      ms: lunar.moonriseMs,
     },
     {
       type: "lunar",
       event: "Moonset",
       time: lunar.moonset,
-      ms: lunar.moonsetMs,
     },
     {
       type: "solar",
       event: "Tomorrow’s Sunrise",
       time: solar.nextSunrise,
-      ms: solar.nextSunriseMs,
     },
     {
       type: "lunar",
       event: "Tomorrow’s Moonrise",
       time: lunar.nextMoonrise,
-      ms: lunar.nextMoonriseMs,
     },
-  ];
-
-  const upcoming = events
+  ]
     .filter(e => e.time && e.time > now)
+    .map(e => ({
+      ...e,
+      ms: e.time.getTime() - now.getTime(),
+    }))
     .sort((a, b) => a.time.getTime() - b.time.getTime());
 
-  return upcoming[0] ?? null;
+  return events[0] ?? null;
 }
