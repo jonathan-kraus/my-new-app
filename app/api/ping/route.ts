e015e1930ae6d182be0e7b621ac2abb8ed8350cb
jonathan-kraus <jonathanckraus@gmail.com>
2026-02-02 14:25:29 -0500

    s7

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,26 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
-import { Axiom } from "@axiomhq/js";
-import crypto from "crypto";
 import { logit } from "@/lib/log/logit";
 import { enrichContext } from "@/lib/log/context";
-import { db } from "@/lib/db";
-import { stat } from "fs";
+
 
 export async function GET(req: NextRequest) {
   const start = Date.now();
   const ctx = await enrichContext(req);
 
   console.log("DB TEST");
-  const result = await db.log.create({
-    data: {
-      level: "info",
-      message: "Test log from dev",
-      domain: "test",
-      payload: {},
-      meta: {},
-    },
-  });
-  console.log("DB TEST RESULT", result);
+
+  console.log("DB TEST RESULT");
 
   await logit(
     "jonathan",
@@ -28,7 +17,7 @@ export async function GET(req: NextRequest) {
       level: "info",
       message: "Ping route hit (axiom test)",
       payload: {
-        uuid: crypto.randomUUID(),
+        uuid: "uuid",
 
         route: "/api/ping",
       },
