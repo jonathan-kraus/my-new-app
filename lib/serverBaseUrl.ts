72f51274280d682e4e2b9856cd6621063ef7cc46
jonathan-kraus <jonathanckraus@gmail.com>
2026-02-07 21:54:27 -0500

    a1

--- lib/serverBaseUrl.ts
+++ lib/serverBaseUrl.ts
@@ -5,5 +5,6 @@ export async function getServerBaseUrl() {
   const h = await headers();
   const host = h.get("x-forwarded-host") ?? h.get("host");
   const proto = h.get("x-forwarded-proto") ?? "https";
+  console.log("host", host, "proto", proto);
   return `${proto}://${host}`;
 }
