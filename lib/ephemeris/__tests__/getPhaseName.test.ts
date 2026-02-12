11245c92777c273f49f36992836b36e25df6c1c7
jonathan-kraus <jonathanckraus@gmail.com>
2026-02-11 20:46:22 -0500

    a3

--- lib/ephemeris/__tests__/getPhaseName.test.ts
+++ lib/ephemeris/__tests__/getPhaseName.test.ts
@@ -1,21 +1,35 @@
-import { getPhaseName } from '../../astronomy/getPhaseName';
+import { getPhaseName } from "../../astronomy/getPhaseName";
 /*
  * @FilePath     : \my-new-app\lib\ephemeris\__tests__\getPhaseName.test.ts
  * @Author       : Jonathan
  * @Date         : 2026-02-11 20:41:55
  * @Description  :
  * @LastEditors  : Jonathan
- * @LastEditTime : 2026-02-11 20:41:55
+ * @LastEditTime : 2026-02-11 20:46:12
  */
 describe("getPhaseName", () => {
-  test("covers major moon phases", () => {
-    expect(getPhaseName(0.0)).toBe("New Moon")
-    expect(getPhaseName(0.1)).toBe("Waxing Crescent")
-    expect(getPhaseName(0.25)).toBe("First Quarter")
-    expect(getPhaseName(0.4)).toBe("Waxing Gibbous")
-    expect(getPhaseName(0.5)).toBe("Full Moon")
-    expect(getPhaseName(0.6)).toBe("Waning Gibbous")
-    expect(getPhaseName(0.75)).toBe("Last Quarter")
-    expect(getPhaseName(0.9)).toBe("Waning Crescent")
-  })
-})
+	test("covers all illumination ranges", () => {
+		expect(getPhaseName(0)).toBe("New Moon");
+		expect(getPhaseName(1)).toBe("New Moon");
+
+		expect(getPhaseName(10)).toBe("Waxing Crescent");
+		expect(getPhaseName(24)).toBe("Waxing Crescent");
+
+		expect(getPhaseName(25)).toBe("First Quarter");
+		expect(getPhaseName(26)).toBe("First Quarter");
+
+		expect(getPhaseName(30)).toBe("Waxing Gibbous");
+		expect(getPhaseName(49)).toBe("Waxing Gibbous");
+
+		expect(getPhaseName(50)).toBe("Full Moon");
+		expect(getPhaseName(51)).toBe("Full Moon");
+
+		expect(getPhaseName(60)).toBe("Waning Gibbous");
+		expect(getPhaseName(74)).toBe("Waning Gibbous");
+
+		expect(getPhaseName(75)).toBe("Last Quarter");
+		expect(getPhaseName(76)).toBe("Last Quarter");
+
+		expect(getPhaseName(90)).toBe("Waning Crescent");
+	});
+});
