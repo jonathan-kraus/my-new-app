24c96828032b5a61a6a7f5be422357e37928183c
jonathan-kraus <jonathanckraus@gmail.com>
2026-02-25 14:31:07 -0500

    l4

--- lib/log/flush.ts
+++ lib/log/flush.ts
@@ -5,15 +5,19 @@ let lastFlushAt: number | null = null;
 export function getLastFlushAt() {
   return lastFlushAt;
 }
-
 export async function flush(batch: any[]) {
   if (!batch.length) return;
 
   console.log("FLUSHING BATCH", batch.length);
 
   try {
-    await client.ingest(process.env.AXIOM_DATASET!, batch);
-    console.log("INGEST COMPLETE");
+    // Send only the actual event objects
+    const events = batch.map(e => e.dataj);
+
+   client.ingest(process.env.AXIOM_DATASET!, events);
+
+    console.log(`FLUSHING BATCH of ${events.length} events`);
+
     lastFlushAt = Date.now();
   } catch (err) {
     console.error("Axiom flush failed", err);
