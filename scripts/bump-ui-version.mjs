f04df8948b31268f95336b0d1c9306ec300cc948
jonathan-kraus <jonathanckraus@gmail.com>
2026-02-28 01:07:33 -0500

    s6

---
+++ scripts/bump-ui-version.mjs
@@ -0,0 +1,15 @@
+/*
+ * @FilePath: \my-new-app\scripts\bump-ui-version.mjs
+ * @LastEditTime: 2026-02-28 01:01:27
+ */
+import { readFileSync, writeFileSync } from "fs";
+
+const path = new URL("../version.json", import.meta.url);
+const json = JSON.parse(readFileSync(path, "utf8"));
+const [major, minor, patch] = json.version.split(".").map(Number);
+const newVersion = `${major}.${minor}.${patch + 1}`;
+
+json.version = newVersion;
+writeFileSync(path, JSON.stringify(json, null, 2) + "\n");
+
+console.log(`Bumped UI version to ${newVersion}`);
