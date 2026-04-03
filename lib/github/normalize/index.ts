47d8cbd071a7220ca328751100d273a3727e3c7d
jonathan-kraus <jonathanckraus@gmail.com>
2026-04-03 03:12:47 -0400

    "Squashing bugs in the Github Normalizer like a boss 💪💥 in lib/github/normalize/index.ts"

--- lib/github/normalize/index.ts
+++ lib/github/normalize/index.ts
@@ -1,6 +1,6 @@
 /*
  * @FilePath: \my-new-app\lib\github\normalize\index.ts
- * @LastEditTime: 2026-04-03 03:07:24
+ * @LastEditTime: 2026-04-03 03:12:08
  */
 // lib/github/normalize/index.ts
 import { BaseNormalizedGitHubEvent, NormalizedGitHubEvent } from "./types";
@@ -24,9 +24,8 @@ export function normalizeGitHubEvent(
   logj({
     domain: "jonathan",
     level: "info",
-    message:
-      "Github normalize processed ",
-    file: "app/api/github-webhook/route.ts",
+    message: "Github normalize event -" + event,
+    file: "lib\github\normalize\index.ts",
     line: 24,
     payload: { repo: repo, event: event },
     meta: { built },
