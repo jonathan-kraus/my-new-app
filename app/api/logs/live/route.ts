ce216c034ec6be9d60be0ee6f5001a0c1cdd2e96
jonathan-kraus <jonathanckraus@gmail.com>
2026-04-24 17:52:32 -0400

    "Unleashed Live Log Route Rocket Fuel 🚀 - app/api/logs/live/route.ts"

--- app/api/logs/live/route.ts
+++ app/api/logs/live/route.ts
@@ -1,13 +1,13 @@
 /*
  * @FilePath: \my-new-app\app\api\logs\live\route.ts
- * @LastEditTime: 2026-04-24 17:48:40
+ * @LastEditTime: 2026-04-24 17:52:16
  */
 import { NextResponse } from "next/server";
 import { queryAxiom } from "@/lib/axiom/query";
 
 export async function GET() {
 const q = `
-  ['domain' != null]
+  ['myapp_logs']
   | sort by _time desc
   | limit 10
 `;
