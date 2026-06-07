d59026072bc02b04f52ae50c106a4b35cdaa491a
jonathan-kraus <jonathanckraus@gmail.com>
2026-06-06 23:55:23 -0400

    "Belt and suspenders tightened 🥳 (eslint.config.mjs)"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+  files: [".github/scripts/*.js"],
+  rules: {
+    "react/display-name": "off",
+  },
+},
 ]);
 
 export default eslintConfig;
