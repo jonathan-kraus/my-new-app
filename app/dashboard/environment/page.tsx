8845fd0290b8713e9dddfab84801147883f05f92
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-19 17:49:48 -0400

    "Refreshed Dashboard and Note Pages 🚀, Let's jot down some laughs!"

--- app/dashboard/environment/page.tsx
+++ app/dashboard/environment/page.tsx
@@ -1,10 +1,10 @@
 /*
  * @FilePath: \my-new-app\app\dashboard\environment\page.tsx
- * @LastEditTime: 2026-06-18 09:37:47
+ * @LastEditTime: 2026-07-19 17:46:24
  */
 // app/dashboard/environment/page.tsx
 import { EnvironmentStatus } from "../components/EnvironmentStatus";
-
+export const metadata = { title: "Environment" };
 export default function EnvironmentPage() {
   return (
     <div className="p-6">
