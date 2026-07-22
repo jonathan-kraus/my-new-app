3342d3fa32a09ac76f97252fed5ac4b768176605
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-22 02:34:14 -0400

    "Bling-bling! ✨ Dashboard page UI polish"

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,36 +6,41 @@ import VersionCard from "@/app/components/dashboard/version-card";
 import { logj } from "@/lib/log/logj";
 import { buildUniversalContext } from "@/lib/log/build-universal-context";
 import type { Metadata } from "next";
+import { auth } from "@/auth";
 
 //import { GitHubCard } from "./components/GitHubCard";
 //import { WeatherCard } from "./components/WeatherCard";
 //import { LogsCard } from "./components/LogsCard";
+import { Session } from '../../lib/generated/prisma/browser';
 export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = { title: "Dashboard" };
 let jei = 0;
 
 export default async function DashboardPage(req: Request) {
   const built = await buildUniversalContext(req as any, "DASHBOARD");
   console.log("DashboardPage built context:", built);
+  let session = await auth();
+
+    
   await logj({
     domain: "dashboard",
     level: "info",
     message: "Dashboard page loaded",
     file: "app/dashboard/page.tsx",
     line: 19,
-    payload: { title: metadata.title, some: "Dashboard page loaded" },
+    payload: { title: metadata.title, a: "b", user: session?.user },
     meta: { built: { ...built, eventIndex: ++jei } },
   });
 
   const data = await getDashboardData();
 
-  console.log("DashboardPage fetched data:", data);
   await logj({
     domain: "dashboard",
     level: "info",
     message: "Dashboard page data fetched",
     file: "app/dashboard/page.tsx",
-    line: 31,
+    line: 32,
     payload: { data: data },
     meta: { built: { ...built, eventIndex: ++jei } },
   });
