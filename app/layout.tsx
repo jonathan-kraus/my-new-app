cbad73748b7f48873c6efe747af064691527096b
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-23 02:46:47 -0400

    "Refreshed App's Layout like a Spring Cleaning at a Minimalist Art Gallery 🎨🌸 #UIupdate"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 // app/layout.tsx
-// import SideNav from "@/app/components/SideNav";
+import SideNav from "@/app/components/SideNav";
 import ClientLayout from "@/app/ClientLayout";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" className="dark">
       <body className="bg-blue-950 text-white min-h-screen antialiased">
         <div className="flex shrink-0 min-h-screen">
-          {/* <SideNav /> */}
+          <SideNav />
           <ClientLayout>{children}</ClientLayout>
         </div>
 
