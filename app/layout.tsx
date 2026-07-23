bb4bfe152596038b4b8c3d371dceb2e8804fac38
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-23 02:20:24 -0400

    "🚀 TSX Time Travel: Warped layout for better UIs"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 // app/layout.tsx
 // import SideNav from "@/app/components/SideNav";
-// import ClientLayout from "@/app/ClientLayout";
+import ClientLayout from "@/app/ClientLayout";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body className="bg-blue-950 text-white min-h-screen antialiased">
         <div className="flex shrink-0 min-h-screen">
           {/* <SideNav /> */}
-          {/* <ClientLayout>{children}</ClientLayout> */}
+          <ClientLayout>{children}</ClientLayout>
         </div>
 
         <Toaster position="top-right" />
