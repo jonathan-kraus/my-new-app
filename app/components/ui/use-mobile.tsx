8d624abf298a415af075fc9a3e64c77ba9fd148d
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-22 23:48:39 -0400

    "UI Spruce Up: Polished Components Galore 💅✨"

--- app/components/ui/use-mobile.tsx
+++ app/components/ui/use-mobile.tsx
@@ -1,1 +1,2 @@
+"use client";
 import * as React from "react";
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+"use client";
 // app/layout.tsx
-import SideNav from "@/app/components/SideNav";
-import ClientLayout from "@/app/ClientLayout";
+// import SideNav from "@/app/components/SideNav";
+// import ClientLayout from "@/app/ClientLayout";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
@@ -13,8 +14,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" className="dark">
       <body className="bg-blue-950 text-white min-h-screen antialiased">
         <div className="flex shrink-0 min-h-screen">
-          <SideNav />
-          <ClientLayout>{children}</ClientLayout>
+          {/* <SideNav /> */}
+          {/* <ClientLayout>{children}</ClientLayout> */}
         </div>
 
         <Toaster position="top-right" />
