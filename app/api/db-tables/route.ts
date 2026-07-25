72277d33caba597d2ecf51819ca1d0bbf8e3fb94
jonathan-kraus <jonathanckraus@gmail.com>
2026-07-24 23:14:32 -0400

    "DB-ROUTE Rewrote the dance of data for a smoother API journey 🕺️"

--- app/api/db-tables/route.ts
+++ app/api/db-tables/route.ts
@@ -1,8 +1,11 @@
 /*
  * @FilePath: \my-new-app\app\api\db-tables\route.ts
- * @LastEditTime: 2026-07-24 21:58:47
+ * @LastEditTime: 2026-07-24 23:13:10
  */
 import { neon } from "@neondatabase/serverless";
+import { logj } from "@/lib/log/logj";
+import { buildUniversalContext } from "@/lib/log/build-universal-context";
+import { NextRequest } from "next/server";
 
 const db = neon(process.env.DATABASE_URL!);
 type FormattedTable = {
@@ -15,7 +18,20 @@ type FormattedTable = {
   };
 };
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const built = await buildUniversalContext(req, "dbtables");
+  let jei = 0;
+  await logj({
+    domain: "jonathan",
+    level: "info",
+    message: `API db-tables started`,
+    file: "app/api/db-tables/route.ts",
+    line: 24,
+    payload: {
+      some: "data",
+    },
+    meta: { built: { ...built, eventIndex: ++jei } },
+  });
   const tables = (await db`
     SELECT table_name
     FROM information_schema.tables
@@ -28,7 +44,17 @@ export async function GET() {
   for (const { table_name } of tables) {
     // Safe identifier quoting
     const quoted = `"${table_name.replace(/"/g, '""')}"`;
-
+    await logj({
+      domain: "jonathan",
+      level: "info",
+      message: `API db-tables count rows for table: ${table_name}`,
+      file: "app/api/db-tables/route.ts",
+      line: 47,
+      payload: {
+        table_name: table_name,
+      },
+      meta: { built: { ...built, eventIndex: ++jei } },
+    });
     // Count rows
     const countRows = (await db`
       SELECT COUNT(*)::int AS exact_count
@@ -38,6 +64,17 @@ export async function GET() {
     const { exact_count } = countRows[0]!;
 
     // Size metrics
+    await logj({
+      domain: "jonathan",
+      level: "info",
+      message: `API db-tables size metrics for table: ${table_name}`,
+      file: "app/api/db-tables/route.ts",
+      line: 67,
+      payload: {
+        table_name: table_name,
+      },
+      meta: { built: { ...built, eventIndex: ++jei } },
+    });
     const sizeRows = (await db`
       SELECT
         pg_total_relation_size(${quoted}) AS total_bytes,
