0178478b397c78cbd6b5d6da6f7385eecdd35825
jonathan-kraus <jonathanckraus@gmail.com>
2026-08-22 19:53:50 -0400

    "All aboard! 🚀 New stop routes on the express track!"

--- app/api/arrivals/[stop]/route.ts
+++ app/api/arrivals/[stop]/route.ts
@@ -1,14 +1,17 @@
 /*
  * @FilePath: \my-new-app\app\api\arrivals\[stop]\route.ts
- * @LastEditTime: 2026-08-22 19:41:26
+ * @LastEditTime: 2026-08-22 19:53:03
  */
 import { mbta } from "@/lib/mbta";
 
-export async function GET(req: Request, context: { params: { stop: string } }) {
-  const params = context.params;
-  console.log("api/arrivals/[stop]/route.ts loaded");
-  console.log("URL:", req.url);
-  console.log("Params:", params);
+export async function GET(req: Request, context: any) {
+  const ctx = await context;
+  const params = await ctx.params;
+
+  console.log("*api/arrivals/[stop]/route.ts loaded");
+  console.log("*URL:", req.url);
+  console.log("*Params:", params);
+
   const data = await mbta("predictions", { "filter[stop]": params.stop });
   return Response.json(data);
 }
