0e8653f2a9c3f59a3d5bc93f584fb08c18af03a9
jonathan-kraus <jonathanckraus@gmail.com>
2026-08-23 16:11:52 -0400

    "Routes getting 'stop'-tacular at Stops and Arrivals 🚀"

--- app/api/arrivals/[stop]/route.ts
+++ app/api/arrivals/[stop]/route.ts
@@ -1,6 +1,6 @@
 /*
  * @FilePath: \my-new-app\app\api\arrivals\[stop]\route.ts
- * @LastEditTime: 2026-08-22 19:53:03
+ * @LastEditTime: 2026-08-23 16:10:38
  */
 import { mbta } from "@/lib/mbta";
 
@@ -12,6 +12,12 @@ export async function GET(req: Request, context: any) {
   console.log("*URL:", req.url);
   console.log("*Params:", params);
 
-  const data = await mbta("predictions", { "filter[stop]": params.stop });
+  console.log("*Fetching MBTA data");
+  const data = await mbta("predictions", {
+    "filter[stop]": params.stop,
+    include: "trip",
+  });
+  console.log("Arrivals response:", JSON.stringify(data, null, 2));
+
   return Response.json(data);
 }
--- app/api/stops/[stop]/route.ts
+++ app/api/stops/[stop]/route.ts
@@ -1,10 +1,10 @@
 /*
  * @FilePath: \my-new-app\app\api\stops\[stop]\route.ts
- * @LastEditTime: 2026-08-23 15:53:36
+ * @LastEditTime: 2026-08-23 16:11:11
  */
 
 import { NextResponse } from "next/server";
-
+console.log("*api/stops/[stop]/route.ts loaded");
 export async function GET(
   _req: Request,
   { params }: { params: { stop: string } },
@@ -13,6 +13,7 @@ export async function GET(
 
   const res = await fetch(url);
   const data = await res.json();
+  console.log("Stop details:", JSON.stringify(data, null, 2));
 
   return NextResponse.json(data);
 }
