af0c5bb690f10aefe68bdff6c7ab9647cfef401e
jonathan-kraus <jonathanckraus@gmail.com>
2026-08-23 22:51:22 -0400

    "🚀 Arrival Routes: New Stop Dance-Off 💃🕺"

--- app/api/arrivals/[stop]/route.ts
+++ app/api/arrivals/[stop]/route.ts
@@ -1,10 +1,11 @@
 /*
  * @FilePath: \my-new-app\app\api\arrivals\[stop]\route.ts
- * @LastEditTime: 2026-08-23 22:33:42
+ * @LastEditTime: 2026-08-23 22:49:34
  */
 import { mbta } from "@/lib/mbta";
 import { logj } from "@/lib/log/client";
 import { staticUniversalContext } from "@/lib/log/buildj";
+import { Axiom } from "@axiomhq/js";
 
 export async function GET(req: Request, context: any) {
   const ctx = await context;
@@ -23,10 +24,11 @@ export async function GET(req: Request, context: any) {
     },
     meta: { built: { ...built, eventIndex: ++jei } },
   });
+  await new Promise((r) => setTimeout(r, 10));
   console.log("*api/arrivals/[stop]/route.ts loaded");
   console.log("*URL:", req.url);
   console.log("*Params:", params);
-
+  await new Promise((r) => setTimeout(r, 10));
   console.log("*Fetching MBTA data");
   const data = await mbta("predictions", {
     "filter[stop]": params.stop,
