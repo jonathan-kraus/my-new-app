38bd27e416d02b4ee3688935ed5f8ff87e1f9d6e
jonathan-kraus <jonathanckraus@gmail.com>
2026-08-23 23:27:41 -0400

    "Blast off! Rocketed routes at [stop] 🚀"

--- app/api/arrivals/[stop]/route.ts
+++ app/api/arrivals/[stop]/route.ts
@@ -1,9 +1,9 @@
 /*
  * @FilePath: \my-new-app\app\api\arrivals\[stop]\route.ts
- * @LastEditTime: 2026-08-23 23:11:26
+ * @LastEditTime: 2026-08-23 23:26:07
  */
 import { mbta } from "@/lib/mbta";
-import { logj } from "@/lib/log/client";
+import { logj } from "@/lib/log/logj";
 import { staticUniversalContext } from "@/lib/log/buildj";
 
 export async function GET(req: Request, context: any) {
