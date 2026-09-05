e1ffb2f5bd84321bdffd11ab4a0a8f62508ed89d
jonathan-kraus <jonathanckraus@gmail.com>
2026-09-05 15:07:22 -0400

    "Bumping digital dashboard swag 🚀 in version-card.tsx - Let's party like it's 2.1.0!"

--- app/components/dashboard/version-card.tsx
+++ app/components/dashboard/version-card.tsx
@@ -1,14 +1,26 @@
 /*
  * @FilePath: \my-new-app\app\components\dashboard\version-card.tsx
- * @LastEditTime: 2026-09-04 01:13:30
+ * @LastEditTime: 2026-09-05 15:05:40
  */
 "use client";
 
 import { getFullPackageData } from "@/lib/version/get-full-package-data";
+import { logj } from "@/lib/log/logj";
+import { buildUniversalContext } from "@/lib/log/build-universal-context";
 
 export default function VersionCard() {
   const data = getFullPackageData();
-
+  const built = buildUniversalContext({} as any, "VERSION_CARD");
+  let jei = 0;
+  logj({
+    domain: "VERSION_CARD",
+    level: "info",
+    message: "Version card loaded",
+    file: "app/components/dashboard/version-card.tsx",
+    line: 17,
+    payload: { data: data },
+    meta: { built: { ...built, eventIndex: ++jei } },
+  });
   const IGNORE_PREFIXES = [
     "@radix-ui/",
     "@types/",
