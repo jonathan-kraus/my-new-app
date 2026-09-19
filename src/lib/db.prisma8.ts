0518ded47a39f917dc4b0c0bb8b826102004cf04
jonathan-kraus <jonathanckraus@gmail.com>
2026-09-19 16:11:37 -0400

    "Swapped Prisma v7 for the cooler v8, keep the data groove alive 🥁💥"

--- src/lib/db.prisma8.ts
+++ src/lib/db.prisma8.ts
@@ -1,14 +1,13 @@
 /*
  * @FilePath: \my-new-app\src\lib\db.prisma8.ts
- * @LastEditors: Jonathan
- * @LastEditTime: 2026-09-15 01:46:48
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2026-09-19 16:02:49
  */
 import "temporal-polyfill/full/global";
 import "dotenv/config";
 import postgres from "@prisma/orm-postgres/runtime";
 import type { Contract } from "../../generated/prisma8/contract.d";
 import contractJson from "../../generated/prisma8/contract.json" with { type: "json" };
-console.log("DB8 initialized");
 export const db8 = postgres<Contract>({
   contractJson,
   url: process.env["DATABASE_URL"]!,
